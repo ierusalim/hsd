b59089ae0f15e9f91e657958fbb2a146d470c9c6
ierusalim <alex@ierusalim.com>
2019-08-30 11:11:35 +0300

    vc85

---
+++ src/vc85.php
@@ -0,0 +1,138 @@
+<?php
+namespace ierusalim\hsd;
+
+class vc85
+{
+    private $vc85c;
+    private $int_cp = 'cp1251';
+    public $work_cp = 'utf-8';
+
+    public function __construct($work_cp = 'utf-8')
+    {
+        $this->work_cp = $work_cp;
+        $this->vc85c = '0123456789'
+              . 'ABCDEFGHJKLMNPQRSTUVWXYZ'
+              . 'abcdefghijkmnopqrstuvwxyz'
+              //. 'згджилпфцчшюэ'
+              //. 'БГДЖИЛПФЦЧШЮЯ'
+              . hex2bin('e7e3e4e6e8ebeff4f6f7f8fefdc1c3c4c6c8cbcfd4d6d7d8dedf');
+    }
+
+    public function encode($src, $output_cp = false)
+    {
+        if (!$output_cp) {
+            $output_cp = $this->work_cp;
+        }
+        $enc = $this->encode_cp1251($src);
+        if ($enc !== false && $output_cp != $this->int_cp) {
+            $enc = \mb_convert_encoding($enc, $output_cp, $this->int_cp);
+        }
+        return $enc;
+    }
+
+    public function decode($src, $from_cp = false)
+    {
+        if (!$from_cp) {
+            $from_cp = $this->work_cp;
+        }
+        if ($from_cp != $this->int_cp) {
+            $src = \mb_convert_encoding($src, $this->int_cp, $from_cp);
+        }
+        return $this->decode_cp1251($src);
+    }
+
+    public function encode_cp1251($src)
+    {
+        $first_c = $this->vc85c[0];
+        $wrk_arr = str_split($src, 4);
+        $last_k = count($wrk_arr) - 1;
+        $last_s = $wrk_arr[$last_k];
+        $last_a = 4 - strlen($last_s);
+        if ($last_a) {
+            $last_k--;
+        }
+        for($k = 0; $k <= $last_k; $k++) {
+            $b85 = $this->base85_encode(unpack('N', $wrk_arr[$k])[1]);
+            if ($a = 5 - strlen($b85)) {
+                $b85 = str_repeat($first_c, $a) . $b85;
+            }
+            $wrk_arr[$k]= $b85;
+        }
+        if ($last_a && ($last_a < 4)) { // 1 (if 3 byte), 2 (if 2 byte), 3 (if 1 byte), 4 (empty)
+            $s4 = str_repeat(chr(0), $last_a) . $last_s;
+            $wrk_arr[$k] = str_pad(
+                $this->base85_encode(unpack('N', $s4)[1]),
+                5 - $last_a, $first_c, \STR_PAD_LEFT);
+        }
+        return implode($wrk_arr);
+    }
+
+    public function decode_cp1251($src)
+    {
+        if (!strlen($src)) {
+            return '';
+        }
+
+        $wrk_arr = \str_split($src, 5);
+        $last_k = \count($wrk_arr) - 1;
+        $last_s = $wrk_arr[$last_k];
+        $last_a = 5 - \strlen($last_s);
+        if ($last_a) {
+            $last_k--;
+        }
+        for($k = 0; $k <= $last_k; $k++) {
+            $c4 = pack('N', $this->base85_decode($wrk_arr[$k]));
+            if ($a = 4 - \strlen($c4)) {
+                $c4 = str_repeat(chr(0), $a) . $c4;
+            }
+            $wrk_arr[$k] = $c4;
+        }
+        if ($last_a) { // 1 (if 3 byte), 2 (if 2 byte), 3 (if 1 byte), 4 (if 1 byte)
+            $dec = $this->base85_decode($last_s);
+            $wrk_arr[$k] = \substr(pack('N', $dec), ($last_a < 3) ? $last_a : 3 - 4);
+        }
+        return \implode($wrk_arr);
+    }
+
+    /**
+     * Decode string from base85 to decimal
+     *
+     * @param string $src
+     * @return string|false
+     */
+    private function base85_decode($src)
+    {
+        $r = 0;
+        for ($i = 0; $i < \strlen($src); $i++) {
+            $ch = $src[$i];
+            $c = \strpos($this->vc85c, $src[$i]);
+            if ($c === false) {
+                return false;
+            }
+            $r *= 85;
+            $r += $c;
+        }
+        return $r;
+    }
+
+    /**
+     * Convert from decimal to base85
+     *
+     * @param string $dec
+     * @return string|false
+     */
+    private function base85_encode($dec)
+    {
+        if (!is_numeric($dec)) {
+            return false;
+        }
+        $r = '';
+        while ((integer)$dec > 0) {
+            $dv = $dec / 85;
+            $i = $dec % 85;
+            $dec = $dv;
+            $r .= $this->vc85c[$i];
+        }
+        return \strrev($r);
+    }
+}
